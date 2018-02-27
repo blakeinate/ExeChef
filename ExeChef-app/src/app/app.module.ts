74a9fecfe236f0f802ed5fddf182c422651ed71e
Spaceage1217 <dimeji.faluyi@gmail.com>
2018-02-27 16:34:21 -0600

    Creating Loging page for user

--- ExeChef-app/src/app/app.module.ts
+++ ExeChef-app/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HomeModule } from './home/home.module';
+import {AuthModule} from "./auth/auth.module";
 import { AppComponent } from './app.component';
+
+import {
+  FooterComponent,
+  HeaderComponent,
+} from './shared';
+
+const rootRouting: ModuleWithProviders = RouterModule.forRoot([], { useHash: true });
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthModule,
+    HomeModule,
+    rootRouting,
   ],
   providers: [],
   bootstrap: [AppComponent]
